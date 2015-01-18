85a3b3a26a247506983f441a4491ed3959efe5b4
Frank_Zhang <zhangxun0327@126.com>
2015-01-18 22:54:59 +0800

    加入login扩展，创建认证蓝本，修改model

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -5,6 +5,7 @@ import unittest
 from flask import current_app
 from app import create_app, db
 
+#这是基础test框架，首选运行的，建立测试环境，创建测试app和数据库，一次运行test开头的测试case
 class BasicTestCase(unittest.TestCase):
     def setUp(self):
         self.app=create_app('testing')
